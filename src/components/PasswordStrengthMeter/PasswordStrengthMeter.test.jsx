b6dbb7d9b239f91323cab710984d88ef3cbd3e28
jdivass <julianandre.divas@gmail.com>
2026-05-19 15:04:17 -0600

    Fixed bun run test

--- src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
+++ src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
@@ -1,7 +1,7 @@
 
 import { render, screen } from '@testing-library/react'
 import { PasswordStrengthMeter } from './PasswordStrengthMeter'
-import  userEvent from 'vitest/browser'
+import userEvent from '@testing-library/user-event'
 
 //Tests de renderizado
 describe('PasswordStrengthMeter', () => {
