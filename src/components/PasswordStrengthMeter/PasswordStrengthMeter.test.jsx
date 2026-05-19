ed1b6b47442fda8f75ba4fb6613ef78911f708ff
jdivass <julianandre.divas@gmail.com>
2026-05-19 14:41:50 -0600

    Add more initial tests

--- src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
+++ src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
@@ -1,6 +1,7 @@
 
 import { render, screen } from '@testing-library/react'
 import { PasswordStrengthMeter } from './PasswordStrengthMeter'
+import  userEvent from 'vitest/browser'
 
 describe('PasswordStrengthMeter', () => {
   describe('Renderizado', () => {
@@ -9,9 +10,19 @@ describe('PasswordStrengthMeter', () => {
       expect(screen.getByRole('textbox', { name: /contraseña/i })).toBeInTheDocument()
     })
 
+    // Probar contraseña vacía
     it('renderiza el indicador de fortaleza con estado inicial "vacía"', () => {
       render(<PasswordStrengthMeter />)
       expect(screen.getByText(/vacía/i)).toBeInTheDocument()
+    })
+
+    // Probar contraseña debil
+    it(`escribir una contraseña corta muestra "debil"`, async () => {
+      render(<PasswordStrengthMeter />)
+      const user = userEvent.setup()
+      const textbox = screen.getByRole('textbox', {name:/contrasea/i})
+      await user.type(textbox, '123');
+      expect(screen.getByText(/débil/i)).toBeInTheDocument()
     })
   })
 })
