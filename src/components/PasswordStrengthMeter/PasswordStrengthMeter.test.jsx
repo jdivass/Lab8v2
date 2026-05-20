cb0ed4a536cb9ea95a9e68c9e4fb6cd83b0d6931
jdivass <julianandre.divas@gmail.com>
2026-05-19 18:03:34 -0600

    Fixed bugs to pass all tests

--- src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
+++ src/components/PasswordStrengthMeter/PasswordStrengthMeter.test.jsx
@@ -8,7 +8,7 @@ describe('PasswordStrengthMeter', () => {
   describe('Renderizado', () => {
     it('renderiza un input de contraseña', () => {
       render(<PasswordStrengthMeter />)
-      expect(screen.getAllByLabelText(/contrase/i))
+      expect(screen.getByLabelText(/contrase/i))
     })
 
     // Probar contraseña vacía
@@ -22,7 +22,7 @@ describe('PasswordStrengthMeter', () => {
     it(`escribir una contraseña corta muestra "debil"`, async () => {
       render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, 'hola');
       expect(screen.getByText(/débil/i)).toBeInTheDocument()
     })
@@ -31,52 +31,52 @@ describe('PasswordStrengthMeter', () => {
     it(`escribir una contraseña de 8 caracteres o más sin números ni símbolos muestra "media"`, async () => {
       render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, 'holasoyyo');
       expect(screen.getByText(/media/i)).toBeInTheDocument()
     })
 
     // Probar contraseña fuerte
     it(`escribir una contraseña de 8 caracteres o mas con al menos un numero muestra "fuerte"`, async () => {
-      render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      render(<PasswordStrengthMeter />)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, 'holasoyyo1');
       expect(screen.getByText(/fuerte/i)).toBeInTheDocument()
     })
 
     // Probar contraseña muy fuerte
     it(`escribir una contraseña de 8 caracteres o mas con numero y simbolo muestra "muy fuerte"`, async () => {
-      render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
-      await user.type(textbox, 'holasoyyo');
+      render(<PasswordStrengthMeter />)
+      const textbox = screen.getByLabelText(/contrase/i)
+      await user.type(textbox, 'holasoyyo12#@');
       expect(screen.getByText(/muy fuerte/i)).toBeInTheDocument()
     })
 // Tests de edge cases
     //Contraseña no debe ser considerada debil
     it(`Una contraseña de 8 caracteres sin numeros no debe ser considerada debil`, async () => {
-      render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      render(<PasswordStrengthMeter />)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, 'holaaaaa');
-      expect(screen.getByText(/débil/i)).not.toBeInTheDocument()
+      expect(screen.queryByText(/débil/i)).not.toBeInTheDocument()
     })
 
     //Contraseña no debe ser considerada media
     it(`Una contraseña de exactamente 7 caracteres no debe ser considerada media`, async () => {
-      render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      render(<PasswordStrengthMeter />)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, 'holaaaa');
-      expect(screen.getByText(/débil/i)).not.toBeInTheDocument()
+      expect(screen.queryByText(/media/i)).not.toBeInTheDocument()
     })
 
     //Contraseña sigue siendo debil
     it(`Una contraseña con solo símbolos y menos de 8 caracteres sigue siendo debil`, async () => {
-      render(<PasswordStrengthMeter />)
       const user = userEvent.setup()
-      const textbox = screen.getAllByLabelText(/contrase/i)
+      render(<PasswordStrengthMeter />)
+      const textbox = screen.getByLabelText(/contrase/i)
       await user.type(textbox, '#$/()');
       expect(screen.getByText(/débil/i)).toBeInTheDocument()
     })
--- src/components/PasswordStrengthMeter/getPasswordStrength.js
+++ src/components/PasswordStrengthMeter/getPasswordStrength.js
@@ -2,13 +2,18 @@
 const getPasswordStrength = (password) => {
     if (password.length === 0) return "vacía"
 
+    if (password.length == 7) return "débil"
+
+    if (password.length == 8 && !/\d/.test(password)) return "media"
+    
     if (/\d/.test(password) && /\W/.test(password) && password.length >= 8 ) return "muy fuerte"
 
     if (/\d/.test(password) && password.length >= 8) return "fuerte"
 
     if (!/\d/.test(password) && !/\W/.test(password) && password.length >= 8 ) return "media"
 
-    if (password.length < 8) return "débil"    
+    if (password.length < 8) return "débil"
+    
 }
 
 export default getPasswordStrength
